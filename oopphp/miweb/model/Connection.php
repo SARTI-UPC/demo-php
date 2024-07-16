15dbf2733406405353ca6730b7aa2fffa7cf5fad
ikramBghiel <103026850+ikramBghiel@users.noreply.github.com>
2024-07-16 12:15:35 +0200

    New database connection

--- oopphp/miweb/model/Connection.php
+++ oopphp/miweb/model/Connection.php
@@ -4,8 +4,16 @@ class Connection {
 
     protected function connect(){
         try {
-            //$con = mysqli_connect("localhost","root","","test");
             $con = new PDO('mysql:host=localhost;dbname=test', 'root','');
+            return $con;
+        } catch (PDOException $e) {
+            return "Error!: ". $e->getMessage()."<br>";
+        }
+    }
+    
+    protected function connectEmpresa(){
+        try {
+            $con = new PDO('mysql:host=localhost;dbname=empresa', 'root','');
             return $con;
         } catch (PDOException $e) {
             return "Error!: ". $e->getMessage()."<br>";
