72dd9eea494a0c4de50768108fe1130190fdf8f9
ikramBghiel <103026850+ikramBghiel@users.noreply.github.com>
2024-07-16 14:18:29 +0200

    add select

--- oopphp/miweb/view/factura.php
+++ oopphp/miweb/view/factura.php
@@ -7,5 +7,20 @@
 </head>
 <body>
     <a href="../includes/generatePdf.php">Generar Factura en PDF</a>
+    <form method="post" action="../includes/generatePdf.php">
+        <?php 
+        require "../includes/autoload.models.php";
+        require "../includes/autoload.controlers.php";
+
+        $comandaContr = new ComandaContr();
+        $comandes = $comandaContr->getComandes();
+        ?>
+    <select name="comanda">
+        <?php foreach ($comandes as $comanda) {?>
+            <option value="<?= $comanda['numcomanda'];?>"><?= $comanda['numcomanda'];?></option>
+        <?php }?>
+    </select>
+    <input type="submit" value="Generate PDF" name="generatePdf">
+    </form>
 </body>
 </html>
